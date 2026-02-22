078d16f3dee34347a961e3d3f1a6cfa8e68f59b1
Krish Ghori <krishghori90@gmail.com>
2026-02-22 12:36:57 +0530

    Add some content

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,48 @@
-import { useState } from 'react'
-
-import './App.css'
-import { useRef } from 'react';
+import { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
-  const [time , settime ] = useState(0);
-  let timeref = useRef(null);
-  function handlestart (){
-    timeref.current =setInterval(() => {
-      settime(time =>time +1)
+  const [time, setTime] = useState(0);
+  const timeRef = useRef(null);
+
+  function handleStart() {
+    if (timeRef.current !== null) return;
+
+    timeRef.current = setInterval(() => {
+      setTime((prev) => prev + 1);
     }, 1000);
-
   }
-  function stoptimer(){
-   clearInterval(timeref.current);
-   timeref.current = null ;
+
+  function handleStop() {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
   }
-  function handlereset(){
-    stoptimer();
-    settime(0);
 
-
+  function handleReset() {
+    handleStop();
+    setTime(0);
   }
 
   return (
-    <div>
-      <h1>Time is : {time} secounds</h1>
-      <button onClick={handlestart}>start</button>
-      <br /><br />
-      <button onClick={stoptimer}>stop</button>
-      <br /><br />
-      <button onClick={handlereset}>reset</button>
+    <div className="container">
+      <div className="timer-card">
+        <h1 className="title">Stopwatch</h1>
+        <div className="time">{time} s</div>
+
+        <div className="buttons">
+          <button className="btn start" onClick={handleStart}>
+            Start
+          </button>
+          <button className="btn stop" onClick={handleStop}>
+            Stop
+          </button>
+          <button className="btn reset" onClick={handleReset}>
+            Reset
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
